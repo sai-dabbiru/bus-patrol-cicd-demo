9ef657d266419495c648db894b0cabddbf99f4e3
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-09-01 13:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def get_status():
     return jsonify({"service": "status-checker", "status": "running"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
